67f360348ee9cc7ab4ed21eafb02ec4da1925a57
peakcary <peakcary@163.com>
2017-02-07 10:25:03 +0800

    init

---
+++ src/components/app.vue
@@ -0,0 +1,22 @@
+<template>
+<div class='message'>
+    {{msg}}
+</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            msg: 'Hello from vue-compoents'
+        }
+    }
+}
+</script>
+<style>
+.message {
+    color: red;
+    font-size: 36px;
+    font-weight: bold;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+ 
+import Vue from 'vue/dist/vue.js'
+import App from './components/app.vue';
+
+new Vue({
+    el:'body',
+    components:{App}
+})
